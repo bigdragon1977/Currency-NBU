a0ade20e8a3535987dcc1f36c541a7a8e0fef6db
Kolomoec Andrey <bigdragon1977@gmail.com>
2011-06-26 15:38:04 +0300

    Приложение запускается. Состояние перед созданием меню. 50% первого пункта

--- gen/com/bigdragon/Currency/R.java
+++ gen/com/bigdragon/Currency/R.java
@@ -8,12 +8,33 @@
 package com.bigdragon.Currency;
 
 public final class R {
+    public static final class array {
+        /** <item>Jupiter</item>
+<item>Saturn</item>
+<item>Uranus</item>
+<item>Neptune</item>
+         */
+        public static final int currency=0x7f030000;
+    }
     public static final class attr {
+    }
+    public static final class id {
+        public static final int button1=0x7f050006;
+        public static final int editText1=0x7f050001;
+        public static final int label=0x7f050000;
+        public static final int radio0=0x7f050004;
+        public static final int radio1=0x7f050005;
+        public static final int radioGroup1=0x7f050003;
+        public static final int spinner=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int cu_convert=0x7f040003;
+        public static final int cu_from=0x7f040001;
+        public static final int cu_to=0x7f040002;
+        public static final int currency_prompt=0x7f040004;
     }
 }
--- src/com/bigdragon/Currency/Currency.java
+++ src/com/bigdragon/Currency/Currency.java
@@ -2,14 +2,69 @@ package com.bigdragon.Currency;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 public class Currency extends Activity
 {
+	private EditText text;
+    private TextView labelview;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        labelview = (TextView) findViewById(R.id.label);
+        labelview.setText("Currency GRN\n");
+        text = (EditText) findViewById(R.id.editText1);
+        Spinner s = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.currency, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+    }
+    public void cu_Clics(View view){
+    	switch (view.getId()){
+    	case R.id.button1:
+    		RadioButton fromButton = (RadioButton) findViewById(R.id.radio0);
+    		RadioButton toButton = (RadioButton) findViewById(R.id.radio1);
+    		if (text.getText().length() == 0){
+    		Toast.makeText(this,"Please enter the number ",Toast.LENGTH_LONG).show();
+    		return;
+    		}
+    		float inputValue = Float.parseFloat(text.getText().toString());
+    		if (fromButton.isChecked()){
+    			text.setText(String.valueOf(convertValtoGrn(inputValue)));
+                chengeLabel();
+    		}else{
+    			text.setText(String.valueOf(convertValtoVal(inputValue)));
+                chengeLabel();
+    		}
+    		if(toButton.isChecked()){
+    			text.setText(String.valueOf(convertValtoVal(inputValue)));
+    		}else{
+    			text.setText(String.valueOf(convertValtoGrn(inputValue)));
+    			}
+
+    		break;
+		}
+    }
+    private void chengeLabel(){
+                labelview.setText("Currency US\n");
+    }
+    private float convertValtoGrn(float valut){
+    	return (valut / 8);
+    }
+    private float convertValtoVal(float grn){
+    	return(grn * 8);
     }
 }
